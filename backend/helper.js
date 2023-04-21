6ba89fd016bede247f1fd3bec080d99642e20052
Ashish <ashishbamania95@gmail.com>
2023-04-21 13:55:07 +0100

    changed backend

--- backend/helper.js
+++ backend/helper.js
@@ -1,39 +1,32 @@
-const getComputerChoice = () => {
+export const getComputerChoice = () => {
+  let randomNumber = Math.floor(Math.random() * 3);
 
-    let randomNumber = Math.floor(Math.random() * 3);
-
-    switch (randomNumber) {
-        case 0:
-            return "Rock";
-        case 1:
-            return "Paper";
-        case 2:
-            return "Scissors";
-        default:
-            return "";
-    };
-
+  switch (randomNumber) {
+    case 0:
+      return "Rock";
+    case 1:
+      return "Paper";
+    case 2:
+      return "Scissors";
+    default:
+      return "";
+  }
 };
 
 export const chooseWinner = (userChoice, computerChoice) => {
-
-    if (userChoice === computerChoice) {
-        return "It's a draw!";
-    }
-    else if (userChoice === 'Rock' && computerChoice === 'Paper') {
-        return 'Computer Wins!';
-    } else if (userChoice === 'Rock' && computerChoice === 'Scissors') {
-        return 'You Win! Congrats!';
-    } else if (userChoice === 'Paper' && computerChoice === 'Rock') {
-        return 'You Win! Congrats!';
-    } else if (userChoice === 'Paper' && computerChoice === 'Scissors') {
-        return 'Computer Wins!';
-    } else if (userChoice === 'Scissors' && computerChoice === 'Paper') {
-        return 'You Win! Congrats!';
-    } else if (userChoice === 'Scissors' && computerChoice === 'Rock') {
-        return 'Computer Wins!';
-    }
+  if (userChoice === computerChoice) {
+    return "It's a draw!";
+  } else if (userChoice === "Rock" && computerChoice === "Paper") {
+    return "Computer Wins!";
+  } else if (userChoice === "Rock" && computerChoice === "Scissors") {
+    return "You Win! Congrats!";
+  } else if (userChoice === "Paper" && computerChoice === "Rock") {
+    return "You Win! Congrats!";
+  } else if (userChoice === "Paper" && computerChoice === "Scissors") {
+    return "Computer Wins!";
+  } else if (userChoice === "Scissors" && computerChoice === "Paper") {
+    return "You Win! Congrats!";
+  } else if (userChoice === "Scissors" && computerChoice === "Rock") {
+    return "Computer Wins!";
+  }
 };
-
-export default getComputerChoice;
-
--- backend/server.js
+++ backend/server.js
@@ -1,46 +1,39 @@
 import express from "express";
-import { chooseWinner } from "./helper.js";
-import getComputerChoice from "./helper.js";
+import { chooseWinner, getComputerChoice } from "./helper.js";
 import cors from "cors";
-import bodyParser from "body-parser"
 
 const app = express();
 
 app.use(cors());
-app.use(bodyParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 let userChoice, computerChoice, result;
 
 app.get("/", (req, res) => {
-    res.status(200).send({
-        userChoice,
-        computerChoice,
-        result
-    });
-
+  res.status(200).send({
+    userChoice,
+    computerChoice,
+    result,
+  });
 });
 
 app.post("/", (req, res) => {
+  userChoice = req.body.userChoice;
 
-    userChoice = req.body.userChoice;
+  computerChoice = getComputerChoice();
 
-    computerChoice = getComputerChoice();
+  console.log(`User Choice : ${userChoice}`);
 
-    console.log(`User Choice : ${userChoice}`);
+  console.log(`Computer Choice : ${computerChoice}`);
 
-    console.log(`Computer Choice : ${computerChoice}`);
+  result = chooseWinner(userChoice, computerChoice);
 
-    result = chooseWinner(userChoice, computerChoice);
+  console.log(`Result : ${result}`);
 
-    console.log(`Result : ${result}`);
-
-    res.status(201).send("Received!");
-
+  res.status(200).send("Received!");
 });
-
 
 app.listen(4000, () => {
-    console.log("Server is running on Port 4000");
+  console.log("Server is running on Port 4000");
 });
-
-
